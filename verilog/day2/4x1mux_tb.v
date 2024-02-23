b8dacef09e273b9fe47655e140a7bfda68247cae
sivasaravanan-verilog <160817543+sivasaravanan-verilog@users.noreply.github.com>
2024-02-22 23:42:07 -0800

    Create 4x1mux_tb.v

---
+++ verilog/day2/4x1mux_tb.v
@@ -0,0 +1,25 @@
+module mux2to1_tb;
+  reg D0,D1,D2,D3;
+  reg S0,S1;
+  wire Y;
+  mux2to1 mm(.D0(D0),.D1(D1),.D2(D2),.D3(D3),.S0(S0),.S1(S1),.Y(Y));
+  initial  begin
+    $dumpfile("mux2to1.vcd");
+    $dumpvars(0,mux2to1_tb);
+  end
+  initial begin
+    D0=2'B00;D1=2'B01;D2=2'B11;D3=2'B11;
+    S0= 1'B0;S1=1'B0;
+    #10;
+    S0= 1'B0;S1=1'B1;
+    #10;
+    S0= 1'B1;S1=1'B0;
+    #10;
+    S0= 1'B1;S1=1'B1;
+    #10;
+    $finish;
+  end
+    initial begin
+      $monitor("time=0%t, D0=%b D1=%b D2=%B D3=%B S0=%b S1=%B Y=%b", $time,D0,D1,D2,D3,S0,S1,Y);
+  end
+endmodule
