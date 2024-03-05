0ba1af4cc265ca8fedb92c7cd56326e798f2c780
sivasaravanan-verilog <160817543+sivasaravanan-verilog@users.noreply.github.com>
2024-03-05 15:27:08 +0530

    Create upcounter.v

---
+++ verilog/day5/upcounter.v
@@ -0,0 +1,16 @@
+module up_count(
+input clk,rst,
+input up,
+  output reg[2:0] cnt);
+  always@(posedge clk) begin
+    if(rst) begin
+      cnt= 3'b0;
+    end
+    else begin
+      if(up) cnt<= cnt+1'b1;
+       
+    end
+  end
+endmodule
+
+      
