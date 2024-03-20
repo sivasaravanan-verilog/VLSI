d3662391512161b094f4d4f44e5e119cb477f678
sivasaravanan-verilog <160817543+sivasaravanan-verilog@users.noreply.github.com>
2024-03-20 16:31:40 +0530

    Update 4x1mux.v

--- verilog/day2/4x1mux.v
+++ verilog/day2/4x1mux.v
@@ -1,4 +1,4 @@
-module mux2to1(
+module mux4to1(
   input D0,
   input D1,
   input D2,
@@ -9,7 +9,7 @@ module mux2to1(
   wire a1,a2,a3,a4; // and gate outputs
   wire inv1,inv2; // inverter output
   not not_1(inv1,S0);
-  not noy_2(inv2,S1);
+  not not_2(inv2,S1);
   and and_1(a1,inv1,inv2,D0);
   and and_2(a2,inv1,S1,D1);
   and and_3(a3,S0,inv2,D2);
