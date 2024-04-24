0edf304333471ea7916fa2ad7212381a11f418f8
sivasaravanan-verilog <160817543+sivasaravanan-verilog@users.noreply.github.com>
2024-04-24 14:36:00 +0530

    queue.sv

---
+++ sysytemverilog/day3/queue.sv
@@ -0,0 +1,27 @@
+module test_queue;
+  int j;
+  int q[$]={0,2,5};
+  initial begin
+    j=1;
+    q.insert(1,j);
+    $display(q);
+    q.delete(1);
+    $display(q);
+    q.push_front(7);
+    $display(q);
+    q.push_back(9);
+    $display(q);
+    q.pop_back();
+    $display(q);
+    q.pop_front();
+    $display(q);
+    q.reverse();
+    $display(q);
+    q.sort();
+    $display(q);
+    q.rsort();
+    $display(q);
+    q.shuffle();
+    $display(q);
+  end
+endmodule
