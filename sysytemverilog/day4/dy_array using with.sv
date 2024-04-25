da2ca9df47d591a39f66a8092545275bd47d1c6c
sivasaravanan-verilog <160817543+sivasaravanan-verilog@users.noreply.github.com>
2024-04-25 10:19:59 +0530

    Update dy_array using with.sv

--- sysytemverilog/day4/dy_array using with.sv
+++ sysytemverilog/day4/dy_array using with.sv
@@ -1,11 +1,15 @@
 module fixed_array;
   int a[5]='{1,2,3,4,5};
+  int a2[7]= '{3,7,5,6,8,9,4};
+  
   int f_sum, f_product;
   initial begin
-    f_sum = a.sum with (item *2);
-    $display("sum of the a \t %0d",f_sum);
+    f_sum = a2.sum with (item *2);
+    $display("sum of the a2 \t %0d",f_sum);
     
     f_product = a.product with (item+2);
     $display("product of a \t%0d",f_product);
+    
+    
   end
 endmodule
