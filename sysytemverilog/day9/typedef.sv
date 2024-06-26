eb3bfd3bfedc614d83477a11b235f4b7e281f911
sivasaravanan-verilog <160817543+sivasaravanan-verilog@users.noreply.github.com>
2024-06-25 23:33:05 -0700

    Create typedef.sv

---
+++ sysytemverilog/day9/typedef.sv
@@ -0,0 +1,23 @@
+
+typedef class karthik;
+  
+  class kaviya;
+    karthik k;
+  endclass
+  
+  class karthik;
+    kaviya v;
+  endclass
+  
+  
+  module love;
+    
+    initial begin
+      karthik c1;
+      kaviya v1;
+      
+      $display("*******************");
+      $display("\n typedef class");
+      $display("\n remove compiler error");
+    end
+  endmodule
